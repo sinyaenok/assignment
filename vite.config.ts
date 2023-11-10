9cb4fd397cb8fb9db732537a3e7233133c3f9455
sinyaenok <dpshrdl56@gmail.com>
2023-11-10 12:57:15 +0900

    update:baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/newassi/",
+  base: "/assignment/",
   resolve: {
     alias: [{ find: "@components", replacement: "./components" }],
   },
